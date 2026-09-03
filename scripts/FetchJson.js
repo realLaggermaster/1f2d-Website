45f47551740acd7b8608da60be66fe276f4facdc
Laggermaster <realLaggermaster@users.noreply.github.com>
2026-09-03 13:59:06 +0300

    Update fetch script & remove html text

--- scripts/FetchJson.js
+++ scripts/FetchJson.js
@@ -1,15 +1,33 @@
-// fetch("./data/server_news.json")
-//     .then(response => {
-//         if (response.ok) {
-//             return response.json();
-//         }
-//     })
-//     .then(news => {
-//         document.getElementById("news-header").textContent = "News - " + news.Date;
-//         document.getElementById("news-message").textContent = news.message;
-//     })
-//     .catch(error => {
-//         document.getElementById("news-header").textContent = "News - " + "(could not get date)";
-//         document.getElementById("news-message").textContent = "Could not get news";
-//         console.error(error);
-//     });
+fetch("./data/data.json")
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+    })
+    .then(data => {
+
+        // News
+        if (data.news) {
+            document.getElementById("news-header").textContent = "News - " + data.news[0].date;
+            document.getElementById("news-message").textContent = data.news[0].message;
+        }
+
+        // Change log
+        if (data.changelog) {
+            document.getElementById("change-log-header").textContent = "Change log - " + data.changelog[0].date;
+            document.getElementById("change-log-message").textContent = data.changelog[0].message;
+        }
+
+        // Community videos
+        if (data.videos.lastupdated) {
+            document.getElementById("videos-date").textContent = "Last listed " + data.videos.lastupdated.date;
+        }
+
+        // Website last updated
+        if (data.lastupdated) {
+            document.getElementById("website-last-updated").textContent = "Website last updated " + data.lastupdated.date;
+        }
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
