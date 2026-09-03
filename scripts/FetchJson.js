3059f1415345b83bca6e9577231a6ebd23307bc7
Laggermaster <realLaggermaster@users.noreply.github.com>
2026-09-03 13:30:12 +0300

    Unite JSON data & update html for fetch script

--- scripts/FetchJson.js
+++ scripts/FetchJson.js
@@ -1,15 +1,15 @@
-fetch("./data/server_news.json")
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-    })
-    .then(news => {
-        document.getElementById("news-header").textContent = "News - " + news.Date;
-        document.getElementById("news-message").textContent = news.message;
-    })
-    .catch(error => {
-        document.getElementById("news-header").textContent = "News - " + "(could not get date)";
-        document.getElementById("news-message").textContent = "Could not get news";
-        console.error(error);
-    });
+// fetch("./data/server_news.json")
+//     .then(response => {
+//         if (response.ok) {
+//             return response.json();
+//         }
+//     })
+//     .then(news => {
+//         document.getElementById("news-header").textContent = "News - " + news.Date;
+//         document.getElementById("news-message").textContent = news.message;
+//     })
+//     .catch(error => {
+//         document.getElementById("news-header").textContent = "News - " + "(could not get date)";
+//         document.getElementById("news-message").textContent = "Could not get news";
+//         console.error(error);
+//     });
